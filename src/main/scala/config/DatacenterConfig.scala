2396399abceddc721e7a5e8b14033eef0b0681b3
scaryface <adityakanthale@gmail.com>
2021-09-16 21:28:53 -0500

    change config variable types

--- src/main/scala/config/DatacenterConfig.scala
+++ src/main/scala/config/DatacenterConfig.scala
@@ -12,15 +12,15 @@ case class DatacenterConfig(configFile: String, configEntry: String) {
 
   // ToDo: Make robust with default values and exception handling
   val hosts = config.getInt("dc.hosts")
-  val vms = config.getLong("dc.vms")
-  val cloudlets = config.getLong("dc.cloudlets")
-  val arch = config.getLong("dc.arch")
-  val os = config.getInt("dc.os")
-  val vmm = config.getInt("dc.vmm")
-  val costPerSecond = config.getInt("dc.costPerSecond")
-  val costPerRam = config.getInt("dc.costPerRam")
-  val costPerStorage = config.getInt("dc.costPerStorage")
-  val costPerBw = config.getInt("dc.costPerBw")
+  val vms = config.getInt("dc.vms")
+  val cloudlets = config.getInt("dc.cloudlets")
+  val arch = config.getString("dc.arch")
+  val os = config.getString("dc.os")
+  val vmm = config.getString("dc.vmm")
+  val costPerSecond = config.getDouble("dc.costPerSecond")
+  val costPerRam = config.getDouble("dc.costPerRam")
+  val costPerStorage = config.getDouble("dc.costPerStorage")
+  val costPerBw = config.getDouble("dc.costPerBw")
 
   logger.debug(s"Datacenter configured successfully for $configEntry")
 }
