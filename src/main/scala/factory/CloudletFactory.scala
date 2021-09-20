9b7f7371355aebf8bffc2a2a70d338e7f4b50d9a
scaryface <adityakanthale@gmail.com>
2021-09-20 17:54:25 -0500

    change type to Cloudlet

--- src/main/scala/factory/CloudletFactory.scala
+++ src/main/scala/factory/CloudletFactory.scala
@@ -1,7 +1,7 @@
 package factory
 
 import config.{CloudletConfig, DatacenterConfig}
-import org.cloudbus.cloudsim.cloudlets.{CloudletAbstract, CloudletSimple}
+import org.cloudbus.cloudsim.cloudlets.{Cloudlet, CloudletAbstract, CloudletSimple}
 import util.CreateLogger
 
 import scala.collection.mutable.ArrayBuffer
@@ -9,27 +9,27 @@ import scala.collection.mutable.ArrayBuffer
 class CloudletFactory
 
 object CloudletFactory:
-  
+
   val logger = CreateLogger(classOf[CloudletFactory])
-  
+
   /**
    * Creates a cloudlet with given configuration
    * @param cloudletConfig Cloudlet configuration object
    **/
-  def createCloudlet(cloudletConfig: CloudletConfig): CloudletAbstract = {
+  def createCloudlet(cloudletConfig: CloudletConfig): Cloudlet = {
     return new CloudletSimple(cloudletConfig.size, cloudletConfig.numPes,
       CloudletUtilizationFactory.createCloudletUtilModel(cloudletConfig))
   }
-  
+
   /**
    * Creates a list of cloudlets with given configuration
    * @param cloudletConfig cloudlet configuration object
-   * @param datacenterConfig datacenter configuration to get the number of cloulets to be created 
+   * @param datacenterConfig datacenter configuration to get the number of cloulets to be created
    **/
   def createCloudlets(cloudletConfig: CloudletConfig,
-                      datacenterConfig: DatacenterConfig): ArrayBuffer[CloudletAbstract] = {
+                      datacenterConfig: DatacenterConfig): ArrayBuffer[Cloudlet] = {
     return ArrayBuffer.tabulate(datacenterConfig.cloudlets)(i => {
       createCloudlet(cloudletConfig)
     })
   }
-  
+
