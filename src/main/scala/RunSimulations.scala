433d388e14596acfbf2aae12664e1e683eb10d3b
scaryface <adityakanthale@gmail.com>
2021-09-21 18:27:54 -0500

    execute new simulations

--- src/main/scala/RunSimulations.scala
+++ src/main/scala/RunSimulations.scala
@@ -2,18 +2,14 @@ import util.{CreateLogger, ObtainConfigReference}
 import simulations.BasicCloudSimPlusExample
 import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
+import simulations.analysis.CloudletUtilizationAnalysis
 import simulations.scheduling.RoundRobinVmScheduler
 
 object RunSimulations:
   val logger = CreateLogger(classOf[RunSimulations])
 
   @main def runSimulation =
-    logger.info("Constructing a cloud model...")
-    BasicCloudSimPlusExample.Start()
-    logger.info("Finished cloud simulation...")
 
-    logger.info("Constructing a cloud model...")
-    RoundRobinVmScheduler.Start()
-    logger.info("Finished cloud simulation...")
+    CloudletUtilizationAnalysis.Start()
 
 class RunSimulations
