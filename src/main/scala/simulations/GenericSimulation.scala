37e0300a56131b3cc9cfed3f90a1e4d723bc5f1a
scaryface <adityakanthale@gmail.com>
2021-09-24 15:29:17 -0500

    simulation test case init

--- src/main/scala/simulations/GenericSimulation.scala
+++ src/main/scala/simulations/GenericSimulation.scala
@@ -12,16 +12,19 @@ import scala.collection.mutable.ArrayBuffer
 
 class GenericSimulation(configStruct: ConfigStruct) {
 
-  def start(): Unit = {
+  val logger = CreateLogger(classOf[GenericSimulation])
+  val cloudsim = new CloudSim()
+  val broker0 = new DatacenterBrokerSimple(cloudsim)
 
-    val logger = CreateLogger(classOf[GenericSimulation])
+
+  def start(): Unit = {
 
     SimulationUtils.checkConfig(configStruct.configFile, configStruct.configEntry)
     logger.info("Running configuration - " + configStruct.configFile)
 
     val config = new ConfigData(configStruct.configFile, configStruct.configEntry)
 
-    val cloudsim = new CloudSim()
+    //val cloudsim = new CloudSim()
 
     //create datacenter with host
     val dc0 = DatacenterFactory.createDatacenter(config.datacenterConfig, config.hostConfig, cloudsim,
@@ -29,7 +32,7 @@ class GenericSimulation(configStruct: ConfigStruct) {
     logger.info(s"Created datacenter: $dc0")
 
     //create broker
-    val broker0 = new DatacenterBrokerSimple(cloudsim)
+    //val broker0 = new DatacenterBrokerSimple(cloudsim)
 
     //create VMs
     val vms = VmFactory.createVms(config.vmConfig, config.hostConfig, config.datacenterConfig)
